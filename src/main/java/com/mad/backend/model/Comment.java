4f9270281ae279ff8ea28ac1e295a7cb116486c7
natasyia <79073609+natasyia00@users.noreply.github.com>
2023-12-03 11:35:52 +0300

    complete comment model

--- src/main/java/com/mad/backend/model/Comment.java
+++ src/main/java/com/mad/backend/model/Comment.java
@@ -1,1 +1,100 @@
 package com.mad.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@Document
+public class Comment{
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    private int ratings;
+    @DBRef
+    private User user;
+    @DBRef
+    private Museum museum;
+    private LocalDateTime date;
+
+    public Comment() {
+	}
+	
+	public Comment(String title, String content, int ratings, User user, Museum museum, LocalDateTime date) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.ratings = ratings;
+		this.user = user;
+		this.museum = museum;
+		this.date = date;
+	}
+
+    public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+    public int getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(int ratings) {
+		this.ratings = ratings;
+	}
+
+     public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+     public Museum getMuseum() {
+		return museum;
+	}
+
+	public void setMuseum(Museum museum) {
+		this.museum = museum;
+	}
+
+     public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+    // ------- might need to make more changes here
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", title=" + title + ", content=" + content +  + ", ratings=" + ratings + 
+         + ", user=" + user + 
+          + ", museum=" + museum + 
+           + ", date=" + date + "]";
+	}
+
+
+}
